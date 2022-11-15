0187a136b4286c487d3b7f2e6b899ed5a1617497
Mathieu Maugin - mathieu.maugin@studio.unibo.it <mathieu.maugin@studio.unibo.it>
2022-11-15 17:38:05 +0100

    Controller.java completed, SimpleGUI.java TO DO

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,52 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private static final String PATH = System.getProperty("user.home")
+                                 + System.getProperty("file.separator")
+                                 + "output.txt";
+    private File file = new File(PATH);
 
+    /**
+     *  Setting a File as current file. 
+     *  @param newFile
+     */
+    public void setFile(final File newFile) {
+        this.file = newFile;
+    }
+
+    /**
+     *  Getting the current file.
+     *  @return the current file.
+     */
+    public File getFile() {
+        return this.file;
+    }
+ 
+    /** 
+     *  Getting the path (in form of String) of the current `File`.
+     *  @return the file path.
+     */
+    public String getPath() {
+        return this.file.getPath();
+    }
+
+    /** 
+     *  Gets a `String` as input and saves its content on the current file. 
+     *  @param content
+     */
+    public void saveString(final String content) {
+        try (PrintStream ps = new PrintStream(this.file, StandardCharsets.UTF_8)) {
+            ps.print(content);
+        } catch (final IOException e) {
+            System.out.println(e.getMessage()); // NOPMD: allowed as this is just an exercise
+        }
+    }
 }
--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,15 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +17,48 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private final JFrame frame = new JFrame("My first Java graphical interface");
+    private static final int PROPORTION = 5;
 
+    /**
+     * Display the application on screen.
+     */
+    private void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    /**
+     * Set all the layout.
+     */
+    public SimpleGUI() {
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        final JTextArea textArea = new JTextArea();
+        final JButton save = new JButton("Save");
+        canvas.add(textArea, BorderLayout.CENTER);
+        canvas.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        /* Handler */
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent arg0) { 
+            }
+        });
+    }
+
+    /**
+     * Start the application.
+     * @param args
+     */
+    public static void main(final String[] args) {
+        new SimpleGUI().display();
+    }
 }
